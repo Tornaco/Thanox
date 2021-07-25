683e733f5683d546bab516e336cdcf547629d5d7
tornaco <tornaco@163.com>
2021-07-25 20:46:46 +0800

    Magisk mod: Add start and bind service fix

--- android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/proxy/ActivityManagerProxyProvider.java
+++ android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/proxy/ActivityManagerProxyProvider.java
@@ -3,8 +3,11 @@ package github.tornaco.android.thanox.magisk.bridge.proxy;
 import static github.tornaco.android.thanox.magisk.bridge.Logging.logging;
 
 import android.app.IActivityManager;
+import android.content.ComponentName;
 import android.os.IBinder;
 import android.os.IInterface;
+
+import com.elvishew.xlog.XLog;
 
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
@@ -34,6 +37,35 @@ public class ActivityManagerProxyProvider implements ProxyProvider, ExceptionTra
                                 if (DEBUG_AMS) {
                                     logging("IActivityManager %s %s", method.getName(), Arrays.toString(args));
                                 }
+
+                                if ("bindIsolatedService".equals(method.getName())) {
+                                    // FIX Service binding error.
+                                    int res = (int) tryInvoke(am, method, args);
+                                    if (res < 0) {
+                                        XLog.w("IActivityManager bindIsolatedService result < 0, we will fix it to 0.");
+                                        res = 0;
+                                    }
+                                    return res;
+                                    // BREAK!!!
+                                }
+
+                                if ("startService".equals(method.getName())) {
+                                    // FIX Service start error.
+                                    ComponentName componentName =
+                                            (ComponentName) tryInvoke(am, method, args);
+                                    if (componentName == null) return null;
+                                    if (componentName.getPackageName() == null)
+                                        return componentName;
+                                    if (componentName.getPackageName().equals("!")
+                                            || componentName.getPackageName().equals("?")
+                                            || componentName.getPackageName().equals("!!")) {
+                                        XLog.w("IActivityManager Try to fix startServiceLocked ERROR throw by system!!!");
+                                        return null;
+                                    }
+                                    return componentName;
+                                    // BREAK!!!
+                                }
+
                                 return tryInvoke(am, method, args);
                             });
                 }

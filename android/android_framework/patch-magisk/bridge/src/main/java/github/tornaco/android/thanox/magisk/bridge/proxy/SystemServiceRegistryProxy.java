7c2df9462da1cd04b106dd9f236e6e703a87dd60
tornaco <tornaco@163.com>
2021-07-28 07:50:05 +0800

    Hook ClipboardManager

--- android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/proxy/SystemServiceRegistryProxy.java
+++ android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/proxy/SystemServiceRegistryProxy.java
@@ -1,7 +1,12 @@
 package github.tornaco.android.thanox.magisk.bridge.proxy;
 
 import android.app.SystemServiceRegistry;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.ServiceManager;
 import android.telephony.TelephonyManager;
 
 import com.elvishew.xlog.XLog;
@@ -28,13 +33,38 @@ public class SystemServiceRegistryProxy {
                                 public String getNetworkOperator() {
                                     String res = super.getNetworkOperator();
                                     String caller = context.getPackageName();
-                                    XLog.w("ActivityThreadProxy TelephonyManager getNetworkOperator called %s %s", caller, res);
+                                    XLog.w("SystemServiceRegistryProxy TelephonyManager getNetworkOperator called %s %s", caller, res);
                                     return res;
                                 }
                             };
                         }
                     }
             );
+
+            SystemServiceRegistry.registerContextAwareService(Context.CLIPBOARD_SERVICE, ClipboardManager.class,
+                    (context, serviceBinder) -> {
+                        try {
+                            return new ClipboardManager(context,
+                                    new Handler(Looper.getMainLooper())) {
+                                @Override
+                                public void setPrimaryClip(ClipData clip) {
+                                    String caller = context.getPackageName();
+                                    XLog.w("SystemServiceRegistryProxy ClipboardManager setPrimaryClip called %s", caller);
+                                    super.setPrimaryClip(clip);
+                                }
+
+                                @Override
+                                public ClipData getPrimaryClip() {
+                                    String caller = context.getPackageName();
+                                    XLog.w("SystemServiceRegistryProxy ClipboardManager getPrimaryClip called %s", caller);
+                                    return super.getPrimaryClip();
+                                }
+                            };
+                        } catch (ServiceManager.ServiceNotFoundException e) {
+                            XLog.e("SystemServiceRegistryProxy error create ClipboardManager", e);
+                            return null;
+                        }
+                    });
 
             XposedHelpers.setStaticBooleanField(SystemServiceRegistry.class, "sInitializing", false);
         } catch (Throwable e) {

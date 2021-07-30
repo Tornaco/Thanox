aa3b9be82c6c69d41c7bc7bd5adec299194fe29f
tornaco <tornaco@163.com>
2021-07-30 22:17:33 +0800

    Support getNetwork-XXX fields(share settings with SIM-xxx for now)

--- android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/proxy/TelephonyManagerProxyProvider.java
+++ android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/proxy/TelephonyManagerProxyProvider.java
@@ -1,5 +1,7 @@
 package github.tornaco.android.thanox.magisk.bridge.proxy;
 
+import static github.tornaco.android.thanox.magisk.bridge.Logging.logging;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.telephony.TelephonyManager;
@@ -53,6 +55,16 @@ public class TelephonyManagerProxyProvider {
         }
 
         @Override
+        public String getLine1AlphaTag() {
+            return super.getLine1AlphaTag();
+        }
+
+        @Override
+        public String getLine1AlphaTag(int subId) {
+            return super.getLine1AlphaTag(subId);
+        }
+
+        @Override
         public String getImei() {
             String superRes = super.getImei();
             return getHookedImeiOr(getSlotIndex(), superRes);
@@ -89,6 +101,41 @@ public class TelephonyManagerProxyProvider {
             return getHookedSimCountryIsoOr(super.getSimCountryIso());
         }
 
+        @Override
+        public String getNetworkCountryIso() {
+            return getHookedSimCountryIsoOr(super.getNetworkCountryIso());
+        }
+
+        @Override
+        public String getNetworkCountryIso(int slotIndex) {
+            return getHookedSimCountryIsoOr(super.getNetworkCountryIso(slotIndex));
+        }
+
+        @Override
+        public String getNetworkOperatorName() {
+            return getHookSimOperatorNameOr(super.getNetworkOperatorName());
+        }
+
+        @Override
+        public String getNetworkOperatorName(int subId) {
+            return getHookSimOperatorNameOr(super.getNetworkOperatorName(subId));
+        }
+
+        @Override
+        public String getNetworkOperator() {
+            return getHookSimOperatorOr(super.getNetworkOperator());
+        }
+
+        @Override
+        public String getNetworkOperator(int subId) {
+            return getHookSimOperatorOr(super.getNetworkOperator(subId));
+        }
+
+        @Override
+        public String getNetworkOperatorForPhone(int phoneId) {
+            return getHookSimOperatorOr(super.getNetworkOperatorForPhone(phoneId));
+        }
+
         private String getHookedDeviceIdOr(String elseValue) {
             return getHookFieldProfileOr(PrivacyManager.PrivacyOp.OP_DEVICE_ID,
                     Fields::getDeviceId, elseValue);
@@ -117,25 +164,32 @@ public class TelephonyManagerProxyProvider {
 
         private <T> T getHookFieldProfileOr(int privacyOp, Function<Fields, T> mapper, T orElseValue) {
             try {
+                logging("getHookFieldProfileOr %s %s", privacyOp, opPkgName);
+
                 String callPackageName = opPkgName;
                 if (callPackageName == null) {
+                    logging("getHookFieldProfileOr, callPackageName == null");
                     return orElseValue;
                 }
 
                 IThanos thanos = ThanosManagerNative.getDefault();
                 if (thanos == null) {
+                    logging("getHookFieldProfileOr, thanos == null");
                     return orElseValue;
                 }
                 IPrivacyManager priv = thanos.getPrivacyManager();
                 if (priv == null) {
+                    logging("getHookFieldProfileOr, priv == null");
                     return orElseValue;
                 }
                 if (!priv.isPrivacyEnabled()) {
+                    logging("getHookFieldProfileOr, !isPrivacyEnabled");
                     return orElseValue;
                 }
 
                 boolean enabledUid = priv.isPackageFieldsProfileSelected(callPackageName);
                 if (!enabledUid) {
+                    logging("getHookFieldProfileOr, !enabledUid");
                     return orElseValue;
                 }
 
@@ -143,12 +197,15 @@ public class TelephonyManagerProxyProvider {
                 Fields f = priv
                         .getSelectedFieldsProfileForPackage(callPackageName, privacyOp);
                 if (f == null) {
+                    logging("getHookFieldProfileOr, selected f is null");
                     return orElseValue;
                 }
                 T res = mapper.apply(f);
                 if (res == null) {
+                    logging("getHookFieldProfileOr, mapper.apply is null");
                     return orElseValue;
                 }
+                logging("getHookFieldProfileOr, return cheat value %s %s %s", privacyOp, opPkgName, res);
                 return res;
             } catch (Throwable e) {
                 XLog.e("getHookFieldProfileOr error", e);

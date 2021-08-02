c67cb51b0f3ebc83347b553dbd818b8103e0935c
tornaco <tornaco@163.com>
2021-08-02 22:32:53 +0800

    Add app shell project to make bridge dex

--- android/scripts/build_magisk_patch.sh
+++ android/scripts/build_magisk_patch.sh
@@ -1,4 +1,4 @@
 rm -rf out/
 ./gradlew clean
-./gradlew -no-daemon :android_framework:patch-magisk:bridge:build
+./gradlew -no-daemon :android_framework:patch-magisk:bridge-dex-app:extractBridgeJar
 ./gradlew -no-daemon :android_framework:patch-magisk:module:zipRelease

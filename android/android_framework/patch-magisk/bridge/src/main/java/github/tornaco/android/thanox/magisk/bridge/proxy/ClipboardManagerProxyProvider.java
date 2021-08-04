adadd3c31382ff60f26e3589181a3e6473b30373
tornaco <tornaco@163.com>
2021-08-04 22:34:23 +0800

    Report start op for read and write clipboard

--- android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/proxy/ClipboardManagerProxyProvider.java
+++ android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/proxy/ClipboardManagerProxyProvider.java
@@ -5,9 +5,14 @@ import static github.tornaco.android.thanox.magisk.bridge.Logging.logging;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.os.Binder;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.ServiceManager;
+
+import github.tornaco.android.thanos.core.app.ThanosManager;
+import github.tornaco.android.thanos.core.secure.ops.AppOpsManager;
+import util.Consumer;
 
 public class ClipboardManagerProxyProvider {
     public static ClipboardManager provide(Context context) {
@@ -21,20 +26,43 @@ public class ClipboardManagerProxyProvider {
 
 
     private static class ClipboardManagerProxy extends ClipboardManager {
+        private final Context context;
+
         public ClipboardManagerProxy(Context context)
                 throws ServiceManager.ServiceNotFoundException {
             super(context, new Handler(Looper.getMainLooper()));
+            this.context = context;
         }
 
         @Override
         public ClipData getPrimaryClip() {
             logging("ClipboardManagerProxy getPrimaryClip");
+            ThanosManager.from(context).ifServiceInstalled(new Consumer<ThanosManager>() {
+                @Override
+                public void accept(ThanosManager thanosManager) {
+                    thanosManager.getAppOpsManager()
+                            .onStartOp(new Binder(),
+                                    AppOpsManager.OP_READ_CLIPBOARD,
+                                    Binder.getCallingUid(),
+                                    context.getPackageName());
+                }
+            });
             return super.getPrimaryClip();
         }
 
         @Override
         public void setPrimaryClip(ClipData clip) {
             logging("ClipboardManagerProxy setPrimaryClip");
+            ThanosManager.from(context).ifServiceInstalled(new Consumer<ThanosManager>() {
+                @Override
+                public void accept(ThanosManager thanosManager) {
+                    thanosManager.getAppOpsManager()
+                            .onStartOp(new Binder(),
+                                    AppOpsManager.OP_WRITE_CLIPBOARD,
+                                    Binder.getCallingUid(),
+                                    context.getPackageName());
+                }
+            });
             super.setPrimaryClip(clip);
         }
     }

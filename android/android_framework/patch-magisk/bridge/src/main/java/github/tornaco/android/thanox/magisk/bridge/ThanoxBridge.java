c828a606dd09bfbf1cde1255da09550e66d4ade5
tornaco <tornaco@163.com>
2021-08-20 21:32:39 +0800

    Check android version before execute thanox bridge

--- android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/ThanoxBridge.java
+++ android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/ThanoxBridge.java
@@ -2,14 +2,21 @@ package github.tornaco.android.thanox.magisk.bridge;
 
 import static github.tornaco.android.thanox.magisk.bridge.Logging.logging;
 
+import android.os.Build;
 import android.util.Log;
 
+import github.tornaco.android.thanos.core.util.OsUtils;
 import github.tornaco.android.thanox.magisk.bridge.proxy.SystemPropProxy;
 import github.tornaco.android.thanox.magisk.bridge.proxy.SystemServiceRegistryProxy;
 
 public class ThanoxBridge {
 
     public static void main(String arg) {
+        if (!OsUtils.isROrAbove()) {
+            System.err.println("Not supported android version: " + Build.VERSION.SDK_INT);
+            return;
+        }
+
         logging("ThanoxBridge, Bridge main with args: %s", arg);
         switch (arg) {
             case "system":

6d5a2a0b7693d08c718dab53e6a5732c52bae659
tornaco <tornaco@163.com>
2021-08-25 20:56:46 +0800

    Add submodule: internal

--- android/buildSrc/src/main/kotlin/tornaco/project/android/thanox/ModueConfigs.kt
+++ android/buildSrc/src/main/kotlin/tornaco/project/android/thanox/ModueConfigs.kt
@@ -1,7 +1,12 @@
 package tornaco.project.android.thanox
 
+import org.gradle.api.Action
 import org.gradle.api.Project
 
 fun Project.applyAndroidModuleConfig() {
+    apply {
+        plugin("com.android.library")
+    }
+
 
 }

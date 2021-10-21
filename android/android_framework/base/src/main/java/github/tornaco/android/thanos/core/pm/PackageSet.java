8f4f1565aaae440bd08a3b39baed3e31fc6e5e28
tornaco <tornaco@163.com>
2021-10-21 21:40:01 +0800

    [services] add prebuilt package sets

--- android/android_framework/base/src/main/java/github/tornaco/android/thanos/core/pm/PackageSet.java
+++ android/android_framework/base/src/main/java/github/tornaco/android/thanos/core/pm/PackageSet.java
@@ -5,104 +5,159 @@ import android.os.Parcelable;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import java.util.Objects;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+
 import util.CollectionUtils;
 
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class PackageSet implements Parcelable {
-  @Getter
-  private String label;
-  @Getter
-  private String id;
-  @Getter
-  private long createAt;
-  private List<String> pkgNames;
+    private String label;
+    private String id;
+    private long createAt;
+    private List<String> pkgNames;
 
-  protected PackageSet(Parcel in) {
-    label = in.readString();
-    id = in.readString();
-    createAt = in.readLong();
-    pkgNames = in.createStringArrayList();
-    if (pkgNames == null) {
-      pkgNames = new ArrayList<>();
+    protected PackageSet(Parcel in) {
+        label = in.readString();
+        id = in.readString();
+        createAt = in.readLong();
+        pkgNames = in.createStringArrayList();
+        if (pkgNames == null) {
+            pkgNames = new ArrayList<>();
+        }
     }
-  }
 
-  public List<String> getPkgNames() {
-    if (pkgNames == null) {
-      pkgNames = new ArrayList<>();
+    public PackageSet(String label, String id, long createAt, List<String> pkgNames) {
+        this.label = label;
+        this.id = id;
+        this.createAt = createAt;
+        this.pkgNames = pkgNames;
     }
-    return pkgNames;
-  }
 
-  public void addPackage(String pkg) {
-    if (pkgNames == null) {
-      pkgNames = new ArrayList<>();
+    public PackageSet() {
     }
-    pkgNames.add(pkg);
-  }
 
-  public void removePackage(String pkg) {
-    if (pkgNames == null) {
-      pkgNames = new ArrayList<>();
+    public static PackageSetBuilder builder() {
+        return new PackageSetBuilder();
     }
-    pkgNames.remove(pkg);
-  }
 
-  public int getPackageCount() {
-    return CollectionUtils.sizeOf(pkgNames);
-  }
+    public List<String> getPkgNames() {
+        if (pkgNames == null) {
+            pkgNames = new ArrayList<>();
+        }
+        return pkgNames;
+    }
 
-  public static final Creator<PackageSet> CREATOR = new Creator<PackageSet>() {
+    public void addPackage(String pkg) {
+        if (pkgNames == null) {
+            pkgNames = new ArrayList<>();
+        }
+        pkgNames.add(pkg);
+    }
+
+    public void removePackage(String pkg) {
+        if (pkgNames == null) {
+            pkgNames = new ArrayList<>();
+        }
+        pkgNames.remove(pkg);
+    }
+
+    public int getPackageCount() {
+        return CollectionUtils.sizeOf(pkgNames);
+    }
+
+    public static final Creator<PackageSet> CREATOR = new Creator<PackageSet>() {
+        @Override
+        public PackageSet createFromParcel(Parcel in) {
+            return new PackageSet(in);
+        }
+
+        @Override
+        public PackageSet[] newArray(int size) {
+            return new PackageSet[size];
+        }
+    };
+
     @Override
-    public PackageSet createFromParcel(Parcel in) {
-      return new PackageSet(in);
+    public int describeContents() {
+        return 0;
     }
 
     @Override
-    public PackageSet[] newArray(int size) {
-      return new PackageSet[size];
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(label);
+        parcel.writeString(id);
+        parcel.writeLong(createAt);
+        parcel.writeStringList(pkgNames);
     }
-  };
 
-  @Override
-  public int describeContents() {
-    return 0;
-  }
-
-  @Override
-  public void writeToParcel(Parcel parcel, int i) {
-    parcel.writeString(label);
-    parcel.writeString(id);
-    parcel.writeLong(createAt);
-    parcel.writeStringList(pkgNames);
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PackageSet that = (PackageSet) o;
+        return createAt == that.createAt &&
+                id.equals(that.id);
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    PackageSet that = (PackageSet) o;
-    return createAt == that.createAt &&
-        id.equals(that.id);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, createAt);
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, createAt);
+    }
+
+    public String toString() {
+        return "PackageSet(label=" + this.label + ", id=" + this.id + ", createAt=" + this.createAt + ", pkgNames=" + this.getPkgNames() + ")";
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public long getCreateAt() {
+        return this.createAt;
+    }
+
+    public static class PackageSetBuilder {
+        private String label;
+        private String id;
+        private long createAt;
+        private List<String> pkgNames;
+
+        PackageSetBuilder() {
+        }
+
+        public PackageSet.PackageSetBuilder label(String label) {
+            this.label = label;
+            return this;
+        }
+
+        public PackageSet.PackageSetBuilder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public PackageSet.PackageSetBuilder createAt(long createAt) {
+            this.createAt = createAt;
+            return this;
+        }
+
+        public PackageSet.PackageSetBuilder pkgNames(List<String> pkgNames) {
+            this.pkgNames = pkgNames;
+            return this;
+        }
+
+        public PackageSet build() {
+            return new PackageSet(label, id, createAt, pkgNames);
+        }
+
+        public String toString() {
+            return "PackageSet.PackageSetBuilder(label=" + this.label + ", id=" + this.id + ", createAt=" + this.createAt + ", pkgNames=" + this.pkgNames + ")";
+        }
+    }
 }

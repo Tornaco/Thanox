7240fcac61d110be8797dcd0a4bc4bc4e0837077
tornaco <tornaco@163.com>
2021-11-22 07:27:29 +0800

    [app] increase base version code

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -27,16 +27,7 @@ plugins {
 androidGitVersion {
     prefix = "v"
     codeFormat = "MMNNPP"
-    baseCode = 3030142
-    // 1000000
-    // 191011
-    // First rel version
-    // 1191011
-    // 2041540
-    // 3030123
-    // 3030124 Catch up with row
-    // 3030138 3.0 rc
-    // 3030142 3.0 release
+    baseCode = 3030143
 }
 
 thanoxVersionCode = androidGitVersion.code()

490078f436c4059fe2fa9e0bcac46be06af74519
tornaco <tornaco@163.com>
2021-12-13 22:12:10 +0800

    [app] click on switchbar

--- android/modules/module_common/src/main/java/github/tornaco/android/thanos/widget/SwitchBar.java
+++ android/modules/module_common/src/main/java/github/tornaco/android/thanos/widget/SwitchBar.java
@@ -29,8 +29,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.Switch;
 import android.widget.TextView;
 
 import com.google.android.material.switchmaterial.SwitchMaterial;
@@ -90,6 +88,7 @@ public class SwitchBar extends FrameLayout
         addOnSwitchChangeListener((switchView, isChecked) -> setTextViewLabel(isChecked));
 
         setOnClickListener(this);
+        findViewById(R.id.card).setOnClickListener(this);
 
         setVisibility(INVISIBLE);
         show();

84ed03fada5f7fcce7b43be8e2c4950dd6f17f0e
tornaco <tornaco@163.com>
2022-01-15 12:41:49 +0800

    [magisk] hook ServiceFetchers

---
+++ android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/proxy/ServiceFetcherProxy.java
@@ -0,0 +1,63 @@
+package github.tornaco.android.thanox.magisk.bridge.proxy;
+
+import android.annotation.Nullable;
+import android.annotation.SuppressLint;
+import android.app.SystemServiceRegistry;
+
+import com.elvishew.xlog.XLog;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.Map;
+
+import github.tornaco.android.thanox.magisk.bridge.util.ReflectionUtils;
+
+class ServiceFetcherProxy {
+
+    static void installInto(Map<String, Object> fetcherMap) {
+        for (String name : fetcherMap.keySet().toArray(new String[0])) {
+            Object originalFetcher = fetcherMap.get(name);
+            XLog.d("ServiceFetcherProxy, installInto, originalFetcher: name", name);
+            if (originalFetcher != null) {
+                Object proxyFetcher = newProxy(originalFetcher, name);
+                XLog.d("ServiceFetcherProxy, installInto, proxyFetcher: %s", proxyFetcher);
+                if (proxyFetcher != null) {
+                    fetcherMap.put(name, proxyFetcher);
+                    XLog.d("ServiceFetcherProxy, installed for: %s", name);
+                } else {
+                    XLog.d("ServiceFetcherProxy, installed failed for: %s", name);
+                }
+            }
+        }
+    }
+
+    @Nullable
+    private static Object newProxy(Object originalFetcher, String name) {
+        try {
+            XLog.d("ServiceFetcherProxy newProxy");
+            @SuppressLint("PrivateApi")
+            Class<?> serviceFetcherClass = Class.forName("android.app.SystemServiceRegistry$ServiceFetcher");
+            XLog.d("ServiceFetcherProxy serviceFetcherClass: %s", serviceFetcherClass);
+            Object res = Proxy.newProxyInstance(SystemServiceRegistry.class.getClassLoader(), new Class[]{
+                    serviceFetcherClass
+            }, new InvocationHandler() {
+                @Override
+                public Object invoke(Object object, Method method, Object[] args) throws Throwable {
+                    if (ReflectionUtils.isToStringMethod(method)) {
+                        return "ServiceFetcherProxy@" + name;
+                    }
+                    Object res = method.invoke(originalFetcher, args);
+                    XLog.d("ServiceFetcherProxy invoke: %s %s %s", method.getName(), Arrays.toString(args), res);
+                    return res;
+                }
+            });
+            XLog.d("ServiceFetcherProxy newProxyInstance: %s", res);
+            return res;
+        } catch (Throwable e) {
+            XLog.e("ServiceFetcherProxy newProxy error", e);
+            return null;
+        }
+    }
+}
--- android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/proxy/SystemServiceRegistryHookInstaller.java
+++ android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/proxy/SystemServiceRegistryHookInstaller.java
@@ -7,12 +7,26 @@ import android.telephony.TelephonyManager;
 
 import com.elvishew.xlog.XLog;
 
+import java.util.Map;
+
 import util.XposedHelpers;
 
+@SuppressWarnings("unchecked")
 public class SystemServiceRegistryHookInstaller {
 
     public static void install() {
         XLog.d("SystemServiceRegistryHookInstaller install");
+
+        Map<Class<?>, String> SYSTEM_SERVICE_NAMES
+                = (Map<Class<?>, String>) XposedHelpers.getStaticObjectField(SystemServiceRegistry.class, "SYSTEM_SERVICE_NAMES");
+        XLog.d("SystemServiceRegistryHookInstaller SYSTEM_SERVICE_NAMES= %s size: %s", SYSTEM_SERVICE_NAMES, SYSTEM_SERVICE_NAMES.size());
+
+        Map<String, Object> SYSTEM_SERVICE_FETCHERS
+                = (Map<String, Object>) XposedHelpers.getStaticObjectField(SystemServiceRegistry.class, "SYSTEM_SERVICE_FETCHERS");
+        XLog.d("SystemServiceRegistryHookInstaller SYSTEM_SERVICE_FETCHERS= %s size: %s", SYSTEM_SERVICE_FETCHERS, SYSTEM_SERVICE_FETCHERS.size());
+
+        ServiceFetcherProxy.installInto(SYSTEM_SERVICE_FETCHERS);
+
         // private static final Map<String, ServiceFetcher<?>> SYSTEM_SERVICE_FETCHERS =
         //            new ArrayMap<String, ServiceFetcher<?>>();
         try {

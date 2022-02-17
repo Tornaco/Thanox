bd43b966e93202a2972161d0309945f321dab814
tornaco <tornaco@163.com>
2022-02-17 20:38:04 +0800

    [magisk] install hooks for app process without waiting for application attached

--- android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/ProcessHookInstaller.java
+++ android/android_framework/patch-magisk/bridge/src/main/java/github/tornaco/android/thanox/magisk/bridge/ProcessHookInstaller.java
@@ -1,18 +1,10 @@
 package github.tornaco.android.thanox.magisk.bridge;
 
-import static github.tornaco.android.thanos.core.util.AppUtils.currentApplicationInfo;
-
-import android.app.ActivityThread;
-import android.content.pm.ApplicationInfo;
-
 import com.elvishew.xlog.XLog;
 
 import github.tornaco.android.thanox.magisk.bridge.proxy.SystemServiceRegistryHookInstaller;
-import util.Consumer;
 
 public class ProcessHookInstaller implements ProcessHandler {
-    private static final long WAIT_ACTIVITY_INTERVAL = 50;
-    private static final long WAIT_ACTIVITY_TIMES = 30;
 
     @Override
     public void onSystemServerProcess() {
@@ -26,52 +18,11 @@ public class ProcessHookInstaller implements ProcessHandler {
         // Hooks for Register.
         // It should run early on app process start.
         SystemServiceRegistryHookInstaller.install();
-        waitForActivityThreadAsync(success -> {
-            if (success) {
-                onStartApplication(processName);
-            } else {
-                XLog.w("ProcessHookInstaller, timeout waitForActivityThread for process: %s", processName);
-            }
-        });
-    }
-
-    @SuppressWarnings("BusyWait")
-    private void waitForActivityThreadAsync(Consumer<Boolean> consumer) {
-        new Thread(() -> {
-            int times = 0;
-            while (ActivityThread.currentOpPackageName() == null && times < WAIT_ACTIVITY_TIMES) {
-                try {
-                    Thread.sleep(WAIT_ACTIVITY_INTERVAL);
-                    times += 1;
-                } catch (InterruptedException e) {
-                    // Noop.
-                }
-            }
-            consumer.accept(ActivityThread.currentOpPackageName() != null);
-        }).start();
+        onStartApplication(processName);
     }
 
     private void onStartApplication(String processName) {
         XLog.d("ProcessHookInstaller onStartApplication: %s", processName);
-        ActivityThread am = ActivityThread.currentActivityThread();
-        if (am == null) {
-            XLog.e("ProcessHookInstaller onStartApplication ActivityThread is null");
-            return;
-        }
-
-        ApplicationInfo currentApp = currentApplicationInfo();
-        if (currentApp == null) {
-            XLog.w("ProcessHookInstaller onStartApplication ApplicationInfo is null");
-            return;
-        }
-
-        XLog.d("ProcessHookInstaller onStartApplication, currentApp: %s", currentApp);
-        String pkgName = currentApp.packageName;
-        if (pkgName == null) {
-            XLog.e("ProcessHookInstaller, onStartApplication packageName is null");
-            return;
-        }
-
         AppProcessSystemServiceHookInstaller.install();
     }
 }

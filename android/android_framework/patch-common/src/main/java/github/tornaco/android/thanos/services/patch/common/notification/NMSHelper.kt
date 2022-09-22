9ba13ecaf552483c8522a01c270c62026458c57b
tornaco <tornaco@163.com>
2022-09-22 17:30:36 +0800

    [services] change NMS hook point

--- android/android_framework/patch-common/src/main/java/github/tornaco/android/thanos/services/patch/common/notification/NMSHelper.kt
+++ android/android_framework/patch-common/src/main/java/github/tornaco/android/thanos/services/patch/common/notification/NMSHelper.kt
@@ -1,5 +1,7 @@
 package github.tornaco.android.thanos.services.patch.common.notification
 
+import android.content.ComponentName
+import com.elvishew.xlog.XLog
 import util.XposedHelpers
 
 object NMSHelper {
@@ -8,5 +10,23 @@ object NMSHelper {
             "com.android.server.notification.NotificationManagerService",
             classLoader
         )
+    }
+
+    fun notificationListenersClass(classLoader: ClassLoader): Class<*> {
+        return XposedHelpers.findClass(
+            "com.android.server.notification.NotificationManagerService\$NotificationListeners",
+            classLoader
+        )
+    }
+
+    // ManagedServiceInfo
+    fun getComponentNameFromManagedServiceInfo(info: Any): ComponentName? {
+        return kotlin.runCatching {
+            XposedHelpers.getObjectField(info,
+                "component") as ComponentName
+        }.getOrElse {
+            XLog.e("getComponentNameFromManagedServiceInfo error", it)
+            null
+        }
     }
 }

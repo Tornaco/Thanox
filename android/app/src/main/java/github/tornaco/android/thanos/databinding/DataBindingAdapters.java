52b52e5b9d29e5c812f2adef485ff460f37ee976
tornaco <tornaco@163.com>
2022-10-21 11:10:14 +0800

    [app] code clean up

--- android/app/src/main/java/github/tornaco/android/thanos/databinding/DataBindingAdapters.java
+++ android/app/src/main/java/github/tornaco/android/thanos/databinding/DataBindingAdapters.java
@@ -1,30 +1,19 @@
 package github.tornaco.android.thanos.databinding;
 
-import static github.tornaco.android.thanos.common.CommonDataBindingAdapters.setIconTint;
-
-import android.annotation.SuppressLint;
-import android.util.Pair;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.DrawableRes;
-import androidx.annotation.NonNull;
 import androidx.databinding.BindingAdapter;
-import androidx.databinding.ObservableField;
-import androidx.databinding.ObservableInt;
 import androidx.databinding.ObservableList;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.GenericTransitionOptions;
-import com.google.android.material.button.MaterialButton;
 
 import java.util.List;
-import java.util.Objects;
 
 import github.tornaco.android.thanos.R;
 import github.tornaco.android.thanos.core.pm.AppInfo;
 import github.tornaco.android.thanos.core.process.RunningState;
-import github.tornaco.android.thanos.main.State;
 import github.tornaco.android.thanos.power.StandbyRule;
 import github.tornaco.android.thanos.start.StartRule;
 import github.tornaco.android.thanos.theme.AppThemePreferences;
@@ -33,28 +22,6 @@ import github.tornaco.android.thanos.util.GlideRequest;
 import util.Consumer;
 
 public class DataBindingAdapters {
-
-    @BindingAdapter("android:boostStatusAppsCount")
-    public static void setBoostStatusAppsCount(TextView textView, int count) {
-        textView.setText(String.valueOf(count));
-    }
-
-    @BindingAdapter("android:privacyAppsCount")
-    public static void setPrivacyAppsCount(TextView textView, ObservableInt count) {
-        textView.setText(String.valueOf(count.get()));
-    }
-
-    @SuppressLint("SetTextI18n")
-    @BindingAdapter("android:boostStatusMemPercent")
-    public static void setBoostStatusMemPercent(TextView textView, int percent) {
-        textView.setText(percent + "%");
-    }
-
-    @SuppressLint("SetTextI18n")
-    @BindingAdapter("android:boostStatusStoragePercent")
-    public static void setBoostStatusStoragePercent(TextView textView, ObservableInt percent) {
-        textView.setText(percent.get() + "%");
-    }
 
     @BindingAdapter("android:startRules")
     public static void setStartRules(RecyclerView recyclerView, ObservableList<StartRule> rules) {
@@ -72,20 +39,6 @@ public class DataBindingAdapters {
         if (adapter != null) {
             adapter.accept(rules);
         }
-    }
-
-    @BindingAdapter("android:featureIcon")
-    public static void setFeatureIcon(ImageView imageView, @DrawableRes int res) {
-        imageView.setImageResource(res);
-    }
-
-    @SuppressWarnings({"ConstantConditions", "unchecked"})
-    @BindingAdapter({"android:appsAllowed", "android:appsDisAllowed"})
-    public static void setAppInfoList(RecyclerView recyclerView,
-                                      ObservableList appsAllowed,
-                                      ObservableList appsDisAllowed) {
-        Consumer<Pair<List<AppInfo>, List<AppInfo>>> adapter = (Consumer) recyclerView.getAdapter();
-        adapter.accept(Pair.create(appsAllowed, appsDisAllowed));
     }
 
     @SuppressWarnings({"unchecked", "ConstantConditions"})
@@ -127,44 +80,6 @@ public class DataBindingAdapters {
                     mergeItem.mLastNumProcesses,
                     mergeItem.mLastNumServices);
             textView.setText(description);
-        }
-    }
-
-    @BindingAdapter({"android:thanosStateText"})
-    public static void setThanosStateText(TextView view, @NonNull ObservableField<State> state) {
-        switch (Objects.requireNonNull(state.get())) {
-            case Active:
-                view.setText(view.getResources().getString(R.string.status_active));
-                break;
-            case InActive:
-                view.setText(view.getResources().getString(R.string.status_not_active));
-                break;
-            case RebootNeeded:
-                view.setText(view.getResources().getString(R.string.status_need_reboot));
-                break;
-            default:
-                view.setText(view.getResources().getString(R.string.status_active));
-                break;
-        }
-    }
-
-    @BindingAdapter({"android:thanosStateText"})
-    public static void setThanosStateText(MaterialButton view, @NonNull ObservableField<State> state) {
-        setThanosStateText((TextView) view, state);
-    }
-
-    @BindingAdapter({"android:thanosStateTint"})
-    public static void setThanosStateTint(ImageView view, @NonNull ObservableField<State> state) {
-        switch (Objects.requireNonNull(state.get())) {
-            case Active:
-                setIconTint(view, R.color.md_green_500);
-                break;
-            case InActive:
-                setIconTint(view, R.color.md_red_500);
-                break;
-            case RebootNeeded:
-                setIconTint(view, R.color.md_amber_500);
-                break;
         }
     }
 }

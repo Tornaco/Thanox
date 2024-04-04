2c1a9447f18f599dac86696fceff404cb4f9e431
Tornaco <Tornaco@163.com>
2024-04-04 17:49:05 +0800

    ITask multiple user

--- android/android_framework/base/src/main/java/github/tornaco/android/thanos/core/profile/handle/ITask.java
+++ android/android_framework/base/src/main/java/github/tornaco/android/thanos/core/profile/handle/ITask.java
@@ -1,12 +1,23 @@
 package github.tornaco.android.thanos.core.profile.handle;
 
+import github.tornaco.android.thanos.core.pm.Pkg;
+
 @HandlerName("task")
 public
 interface ITask {
 
     void removeTasksForPackage(String pkgName);
 
+    void removeTasksForPackage(String pkgName, int userId);
+
+    void removeTasksForPackage(Pkg pkg);
+
     boolean hasTaskFromPackage(String pkgName);
+
+    boolean hasTaskFromPackage(String pkgName, int userId);
+
+    boolean hasTaskFromPackage(Pkg pkg);
+
 
     void clearBackgroundTasks();
 }

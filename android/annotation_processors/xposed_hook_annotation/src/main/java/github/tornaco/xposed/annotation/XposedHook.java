59e14b7578492445a6ca118d188991fb412823fe
Tornaco <Tornaco@163.com>
2024-06-28 08:37:47 +0800

    Android 15[1]

--- android/annotation_processors/xposed_hook_annotation/src/main/java/github/tornaco/xposed/annotation/XposedHook.java
+++ android/annotation_processors/xposed_hook_annotation/src/main/java/github/tornaco/xposed/annotation/XposedHook.java
@@ -10,29 +10,32 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.CLASS)
 @Documented
 public @interface XposedHook {
-  int[] targetSdkVersion();
+    int[] targetSdkVersion();
 
-  boolean active() default true;
+    boolean active() default true;
 
-  int priority() default 1;
+    int priority() default 1;
 
-  interface SdkVersions {
-    int _21 = 21;
-    int _22 = 22;
-    int _23 = 23;
-    int _24 = 24;
-    int _25 = 25;
-    int _26 = 26;
-    int _27 = 27;
-    int _28 = 28;
-    int _29 = 29;
-    int _30 = 30;
-    int _31 = 31;
-    int _32 = 32;
-    // Android T
-    int _33 = 33;
-    int _34 = 34;
+    interface SdkVersions {
+        int _21 = 21;
+        int _22 = 22;
+        int _23 = 23;
+        int _24 = 24;
+        int _25 = 25;
+        int _26 = 26;
+        int _27 = 27;
+        int _28 = 28;
+        int _29 = 29;
+        int _30 = 30;
+        int _31 = 31;
+        int _32 = 32;
+        // T
+        int _33 = 33;
+        // U
+        int _34 = 34;
+        // V
+        int _35 = 35;
 
-    int[] ALL = {_21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34};
-  }
+        int[] ALL = {_21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35};
+    }
 }

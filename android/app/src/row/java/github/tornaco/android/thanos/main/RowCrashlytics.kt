d095cb1e9771e2e93ba049db304fb1e78fb77a42
Tornaco <Tornaco@163.com>
2024-08-30 15:19:15 +0800

    setCustomKeys

--- android/app/src/row/java/github/tornaco/android/thanos/main/RowCrashlytics.kt
+++ android/app/src/row/java/github/tornaco/android/thanos/main/RowCrashlytics.kt
@@ -2,21 +2,34 @@ package github.tornaco.android.thanos.main
 
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.crashlytics.setCustomKeys
 import com.google.firebase.ktx.Firebase
 import github.tornaco.android.thanos.core.analytics.Crashlytics
+import github.tornaco.android.thanos.core.app.AppGlobals
+import now.fortuitous.app.donate.DonateSettings
+import tornaco.apps.thanox.subscribe.DeviceId
 
 class RowCrashlytics : Crashlytics {
     private var firebaseCrashlytics: FirebaseCrashlytics? = null
 
     init {
-        firebaseCrashlytics = Firebase.crashlytics
+        firebaseCrashlytics = Firebase.crashlytics.apply {
+            setCustomKeys {
+                key("thanos_device", DeviceId.requireDeviceId())
+                key("thanox_device", DonateSettings.getDeviceId(AppGlobals.context))
+            }
+        }
     }
 
     override fun log(message: String) {
-        firebaseCrashlytics?.log(message)
+        kotlin.runCatching {
+            firebaseCrashlytics?.log(message)
+        }
     }
 
     override fun logError(e: Throwable) {
-        firebaseCrashlytics?.log(e.stackTraceToString())
+        kotlin.runCatching {
+            firebaseCrashlytics?.log(e.stackTraceToString())
+        }
     }
 }

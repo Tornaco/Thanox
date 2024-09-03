f66e164c021c7244fc9ebfd2d89c6c2a1a366bdd
Tornaco <Tornaco@163.com>
2024-09-03 09:34:43 +0800

    Fix MMKV not init err

--- android/app/src/row/java/github/tornaco/android/thanos/main/RowCrashlytics.kt
+++ android/app/src/row/java/github/tornaco/android/thanos/main/RowCrashlytics.kt
@@ -2,22 +2,18 @@ package github.tornaco.android.thanos.main
 
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.crashlytics.ktx.crashlytics
-import com.google.firebase.crashlytics.setCustomKeys
 import com.google.firebase.ktx.Firebase
 import github.tornaco.android.thanos.core.analytics.Crashlytics
-import github.tornaco.android.thanos.core.app.AppGlobals
-import now.fortuitous.app.donate.DonateSettings
-import tornaco.apps.thanox.subscribe.DeviceId
 
 class RowCrashlytics : Crashlytics {
     private var firebaseCrashlytics: FirebaseCrashlytics? = null
 
     init {
         firebaseCrashlytics = Firebase.crashlytics.apply {
-            setCustomKeys {
-                key("thanos_device", DeviceId.requireDeviceId())
-                key("thanox_device", DonateSettings.getDeviceId(AppGlobals.context))
-            }
+//            setCustomKeys {
+//                key("thanos_device", DeviceId.requireDeviceId())
+//                key("thanox_device", DonateSettings.getDeviceId(AppGlobals.context))
+//            }
         }
     }
 

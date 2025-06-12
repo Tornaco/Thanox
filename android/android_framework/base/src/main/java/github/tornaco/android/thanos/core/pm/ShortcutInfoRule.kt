51007043b04792c3186e7b61044592b04a02a61c
Tornaco <Tornaco@163.com>
2025-06-12 15:09:03 +0800

    Update rule

--- android/android_framework/base/src/main/java/github/tornaco/android/thanos/core/pm/ShortcutInfoRule.kt
+++ android/android_framework/base/src/main/java/github/tornaco/android/thanos/core/pm/ShortcutInfoRule.kt
@@ -5,34 +5,46 @@ import github.tornaco.android.thanos.core.util.GsonUtils
 
 @Keep
 data class ShortcutInfoBlockRules(
-    val commonRules: List<CommonRule>,
+    val commonRule: CommonRule,
     val appRules: List<AppRule>,
 ) {
     @Keep
     data class CommonRule(
-        val labelRegex: String
+        val labelRegexes: List<String>
     )
 
     @Keep
     data class AppRule(
         val pkgName: String,
-        val label: String?,
-        val labelRegex: String?,
+        val labels: List<String>,
+        val labelRegexes: List<String>,
     )
 }
 
 fun main() {
     val rules = ShortcutInfoBlockRules(
-        commonRules = listOf(
-            ShortcutInfoBlockRules.CommonRule("Regex1"),
-            ShortcutInfoBlockRules.CommonRule("Regex2"),
-        ),
+        commonRule = ShortcutInfoBlockRules.CommonRule(listOf("卸载", "福气兑红包")),
         appRules = listOf(
             ShortcutInfoBlockRules.AppRule(
-                "com.xx.xx",
-                label = "Label",
-                labelRegex = null
-            )
+                "com.tmall.wireless",
+                labels = listOf("一键清理加速"),
+                labelRegexes = emptyList()
+            ),
+            ShortcutInfoBlockRules.AppRule(
+                "com.shizhuang.duapp",
+                labels = listOf("空间清理"),
+                labelRegexes = emptyList()
+            ),
+            ShortcutInfoBlockRules.AppRule(
+                "com.xunmeng.pinduoduo",
+                labels = listOf("现金可提现", "好礼免费拿"),
+                labelRegexes = emptyList()
+            ),
+            ShortcutInfoBlockRules.AppRule(
+                "me.ele",
+                labels = listOf("清理缓存", "桌面组件中心", "我的红包"),
+                labelRegexes = emptyList()
+            ),
         )
     )
 

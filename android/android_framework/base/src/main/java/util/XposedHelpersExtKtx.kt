add8038d116c96a9276a91cfe42c4dbebb250c7a
Tornaco <Tornaco@163.com>
2025-10-01 15:36:57 +0800

    fix Install

--- android/android_framework/base/src/main/java/util/XposedHelpersExtKtx.kt
+++ android/android_framework/base/src/main/java/util/XposedHelpersExtKtx.kt
@@ -1,11 +1,29 @@
 package util
 
 import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
 
 object XposedHelpersExtKtx {
     fun XC_MethodHook.MethodHookParam.firstArgIndexOfType(type: Class<*>): Int {
         return args.indexOfFirst {
             it.javaClass == type
         }
+    }
+
+    fun hookAllMethods(
+        hookClass: Class<*>,
+        methodNames: Array<String>,
+        callback: XC_MethodHook
+    ): Set<XC_MethodHook.Unhook> {
+        val res = mutableSetOf<XC_MethodHook.Unhook>()
+        methodNames.forEach {
+            runCatching {
+                val resI = XposedBridge.hookAllMethods(
+                    hookClass, it, callback
+                )
+                res.addAll(resI)
+            }
+        }
+        return res
     }
 }
